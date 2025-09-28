0f9ada6c5357490e03c3bfe5169442bc9eac1024
Juliano <63940853+nexcoyocoatl@users.noreply.github.com>
2025-09-28 06:11:20 -0300

    Biblioteca Getch multiplataforma.
    Modificado de: https://code.activestate.com/recipes/134892-getch-like-unbuffered-character-reading-from-stdin

---
+++ getch.py
@@ -0,0 +1,87 @@
+### retirado e modificado de: https://code.activestate.com/recipes/134892-getch-like-unbuffered-character-reading-from-stdin
+# Não foi testado em Windows e MacOS, apenas Linux
+
+class _Getch:
+    """
+    Gets a single character from standard input.  Does not echo to
+    the screen.
+    """
+    def __init__(self):
+        try:
+            self.impl = _GetchWindows()
+        except ImportError:
+            try:
+                self.impl = _GetchMacCarbon()
+            except(AttributeError, ImportError):
+                self.impl = _GetchUnix()
+
+    def __call__(self): return self.impl()
+
+class _GetchUnix:
+    def __init__(self):
+        import tty, sys, termios # import termios now or else you'll get the Unix version on the Mac
+
+    def __call__(self):
+        import sys, tty, termios
+        fd = sys.stdin.fileno()
+        old_settings = termios.tcgetattr(fd)
+        try:
+            tty.setraw(sys.stdin.fileno())
+            ch = sys.stdin.read(1)
+        finally:
+            termios.tcsetattr(fd, termios.TCSADRAIN, old_settings)
+        return ch
+
+class _GetchWindows:
+    def __init__(self):
+        import msvcrt
+
+    def __call__(self):
+        import msvcrt
+        return msvcrt.getch()
+
+class _GetchMacCarbon:
+    """
+    A function which returns the current ASCII key that is down;
+    if no ASCII key is down, the null string is returned.  The
+    page http://www.mactech.com/macintosh-c/chap02-1.html was
+    very helpful in figuring out how to do this.
+    """
+    def __init__(self):
+        import Carbon
+        Carbon.Evt #see if it has this (in Unix, it doesn't)
+
+    def __call__(self):
+        import Carbon
+        if Carbon.Evt.EventAvail(0x0008)[0]==0: # 0x0008 is the keyDownMask
+            return ''
+        else:
+            #
+            # The event contains the following info:
+            # (what,msg,when,where,mod)=Carbon.Evt.GetNextEvent(0x0008)[1]
+            #
+            # The message (msg) contains the ASCII char which is
+            # extracted with the 0x000000FF charCodeMask; this
+            # number is converted to an ASCII character with chr() and
+            # returned
+            #
+            (what,msg,when,where,mod)=Carbon.Evt.GetNextEvent(0x0008)[1]
+            return chr(msg & 0x000000FF)
+
+getch = _Getch()
+
+if __name__ == '__main__': # a little test
+    print('Press a key')
+    inkey = getch
+    import sys
+    # for i in range(sys.maxsize):
+    while(True):
+        k = inkey()
+        if (k == "\033"):
+            getch()
+            k = inkey()
+            print(k)
+        if (k != ''):
+            break
+    print ('you pressed', k)
+###
